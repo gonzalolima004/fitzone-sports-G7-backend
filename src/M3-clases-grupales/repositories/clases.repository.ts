8424f3269d92cd41abb0d05a62894c475a27b091
joaquinribarola <joaquinribarola45@gmail.com>
2026-09-17 17:08:04 -0300

    feat(clases): implementa ClasesRepository y actualiza cliente Prisma a v7 (US-04-01)

---
+++ src/M3-clases-grupales/repositories/clases.repository.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../database/prisma-service/prisma.service';
+import { CreateClaseDto } from '../dto/create-clase.dto';
+import { UpdateClaseDto } from '../dto/update-clase.dto';
+
+@Injectable()
+export class ClasesRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async crearClase(data: CreateClaseDto) {
+    return await this.prisma.clase.create({
+      data,
+    });
+  }
+
+  async obtenerClasesPorSede(id_sede: number) {
+    return await this.prisma.clase.findMany({
+      where: {
+        id_sede,
+        activo: true,
+      },
+    });
+  }
+
+  async obtenerClasePorId(id_clase: number) {
+    return await this.prisma.clase.findUnique({
+      where: {
+        id_clase,
+      },
+    });
+  }
+
+  async actualizarClase(id_clase: number, data: UpdateClaseDto) {
+    return await this.prisma.clase.update({
+      where: {
+        id_clase,
+      },
+      data,
+    });
+  }
+}
