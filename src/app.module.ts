40352593f8589dee0593ef122d6ef47b685fae23
Gonzalo Lima <limagonzalo0@gmail.com>
2026-09-17 12:41:55 -0300

    feat: Tarea 1.2: Integración del SDK de MercadoPago

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import mercadopagoConfig from './config/mercadopago.config';
+import { PagosModule } from './modules/M5-pagos/pagos.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [mercadopagoConfig],
+    }),
+    PagosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
