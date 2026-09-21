985bbfc647eae3c78ee787d3dcfe8958d0a56747
Gonzalo Lima <limagonzalo0@gmail.com>
2026-09-21 12:19:27 -0300

    feat: creación de preferencia de pago

--- src/modules/M5-pagos/pagos.module.ts
+++ src/modules/M5-pagos/pagos.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MercadoPagoService } from './services/mercadopago.service';
 import { PagosRepository } from './repositories/pagos.repository';
+import { PagosService } from './services/pagos.service';
 
 @Module({
-  providers: [MercadoPagoService, PagosRepository],
-  exports: [MercadoPagoService, PagosRepository],
+  providers: [MercadoPagoService, PagosRepository, PagosService],
+  exports: [MercadoPagoService, PagosRepository, PagosService],
 })
 export class PagosModule {}
