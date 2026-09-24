3fea29e6683a6f195a1355134df7310bbcaff0bc
Matías Sillen Ríos <matias.sillen@gmail.com>
2026-09-24 18:06:49 -0300

    feat(module): inyectar las estrategias en el array providers en canchas.modules.ts

--- src/modules/M4-canchas/canchas.module.ts
+++ src/modules/M4-canchas/canchas.module.ts
@@ -3,15 +3,31 @@ import { CanchasController } from './controllers/canchas.controller';
 import { CanchasService } from './services/canchas.service';
 import { CanchasRepository } from './repositories/canchas.repository';
 import { ReservasCanchasRepository } from './repositories/reservas-canchas.repository';
+import { PrecioEstandarStrategy } from './patterns/strategy/precio-estandar.strategy';
+import { DescuentoSocioStrategy } from './patterns/strategy/descuento-socio.strategy';
+import { HorarioPicoStrategy } from './patterns/strategy/horario-pico.strategy';
+import { PrecioContextService } from './services/precio-context.service';
 
 @Module({
   // Los controladores que manejan las rutas (ej. GET /canchas)
   controllers: [CanchasController],
 
   // La lógica de negocio y el acceso a base de datos que se inyectan
-  providers: [CanchasService, CanchasRepository, ReservasCanchasRepository],
-
+  providers: [
+    CanchasService,
+    CanchasRepository,
+    ReservasCanchasRepository,
+    // Registramos las estrategias y su contexto
+    PrecioEstandarStrategy,
+    DescuentoSocioStrategy,
+    HorarioPicoStrategy,
+    PrecioContextService,
+  ],
   // Exportamos el servicio por si otro módulo (ej. Reservas o Pagos) necesita consultarlo en el futuro
-  exports: [CanchasService, ReservasCanchasRepository],
+  exports: [
+    CanchasService,
+    ReservasCanchasRepository,
+    PrecioContextService, // Exportado para usarlo luego en la reserva final
+  ],
 })
 export class CanchasModule {}
