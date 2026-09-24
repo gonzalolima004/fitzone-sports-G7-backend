30b841cf5d7ecb3645dfce9b2fe4dd9624b7e9b5
joaquinribarola <joaquinribarola45@gmail.com>
2026-09-24 17:37:55 -0300

    feat(clases): agrega observer de realtime para notificar vacantes (Tarea 4.5.3)

--- src/modules/M3-clases-grupales/patterns/observer/lista-espera-observer.interface.ts
+++ src/modules/M3-clases-grupales/patterns/observer/lista-espera-observer.interface.ts
@@ -2,6 +2,7 @@ export interface VacanteLiberadaEvento {
   id_clase: number;
   fecha_inicio: Date;
   fecha_fin: Date;
+  id_usuario_promovido?: number;
 }
 
 export interface ListaEsperaObserver {
